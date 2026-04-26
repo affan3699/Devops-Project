0b28ce271209cfcd2fad8df548aed9fd8564ba3c
Affan <affan.murtaza@qubitsinformatics.com>
2026-04-26 15:26:33 +0500

    Update HomeController.java

--- src/main/java/com/example/chatbot/controller/HomeController.java
+++ src/main/java/com/example/chatbot/controller/HomeController.java
@@ -3,11 +3,13 @@ package com.example.chatbot.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class HomeController {
 
     @GetMapping("/api/message")
     public String home() {
-        return "index"; // This will literally return the string "index" to the browser
+        return "Service running" + LocalDateTime.now();
     }
 }
