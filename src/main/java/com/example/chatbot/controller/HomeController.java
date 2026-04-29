cb10b57f4d4972fe778b097d0b7290775f767b7d
Affan <affan.murtaza@qubitsinformatics.com>
2026-04-29 22:43:28 +0500

    Test

--- src/main/java/com/example/chatbot/controller/HomeController.java
+++ src/main/java/com/example/chatbot/controller/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
 
     @GetMapping("/api/message")
     public String home() {
-        logger.info("Service running {}", LocalDateTime.now());
+        logger.info("Service running hello {}", LocalDateTime.now());
         return "Service running " + LocalDateTime.now();
     }
 }
